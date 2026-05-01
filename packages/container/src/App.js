16bd84476a2b9dd249b1e1b32f4bd3f9c102e937
Nikhil Kashid <Nkashid99@gmail.com>
2026-05-01 19:19:34 -0400

    fix: reorder import statements for consistency in App component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { StylesProvider } from "@material-ui/core/styles";
-import { BrowserRouter, createGenerateClassName } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+import { BrowserRouter } from "react-router-dom";
 
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
