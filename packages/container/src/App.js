01160bd4865f692d538e3fbc704c9a1d92ddd517
Nikhil Kashid <Nkashid99@gmail.com>
2026-05-01 18:59:41 -0400

    feat: implement Header component and refactor App structure to include routing and styles

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { StylesProvider } from "@material-ui/core/styles";
+import { BrowserRouter } from "react-router-dom";
 
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+
 
 export default () => {
   return <>
-    <h1>Container App!!!</h1>
-    <hr/>
-    <MarketingApp />
+    <BrowserRouter>
+      <StylesProvider>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   </>;
 };
