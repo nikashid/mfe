8f2e8afdf1e5c3b52ef22025525c7002ccdbb278
Nikhil Kashid <Nkashid99@gmail.com>
2026-05-03 12:20:30 -0400

    fix: correct import casing for SignIn and SignUp components in App

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Switch, Route, Router} from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
+
+import SignIn from './components/Signin';
+import SignUp from './components/Signup';
 
 
 const styleClassGenerator = createGenerateClassName({
